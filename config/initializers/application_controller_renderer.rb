0661035e3c4c3291ffef0eb4e7efe50fc7929408
tetsu05 <tetsu051955@gmail.com>
2021-12-09 12:20:52 +0000

    [Finish]4-3復習

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c0840dacad14050ddab1c9feb4f7d3beb477f15ec7219737a178d45d268404f52bea09f3fba2818f6322dfc140e68028c3bd0242aa549960134b3e43469dd790'
